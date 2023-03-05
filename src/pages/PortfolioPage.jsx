985711af248c64b370df7bbbb2ce94c6b9129b62
Lon Heligas <lon.heligas@gmail.com>
2023-03-04 20:53:18 -0600

    added card functionality to portfolio

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import PortfolioCard from '../components/PortfolioCard';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 const PortfolioPage = (props) => {
 
   return (
     <>
     <h1>Portfolio</h1>
+    <CardGroup>
+      <PortfolioCard />  
+      <PortfolioCard />  
+      <PortfolioCard />  
+      <PortfolioCard />  
+    </CardGroup>
     </>
   );
 }
