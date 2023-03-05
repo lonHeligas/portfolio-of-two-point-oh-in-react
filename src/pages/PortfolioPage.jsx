c51f401af804c39f989d1d7fbe99dbc9c590cec6
Lon Heligas <lon.heligas@gmail.com>
2023-03-05 10:58:23 -0600

    got the title working for the card data

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import PortfolioCard from '../components/PortfolioCard';
 import CardGroup from 'react-bootstrap/CardGroup';
+import ProfileCreatorApp from '../portfolio-data/ProfileCreatorAppData';
 
 const PortfolioPage = (props) => {
 
   return (
     <>
     <h1>Portfolio</h1>
-    <CardGroup>
-      <BridgeClock />
-      <PortfolioCard />  
-      <PortfolioCard />  
-      <PortfolioCard />  
+    <CardGroup>     
+      {ProfileCreatorApp.map(info => <PortfolioCard {...info} />)}       
     </CardGroup>
     </>
   );
 }
 
 
-const BridgeClock = () => {
-  <PortfolioCard title="portfoliio piece 1 "></PortfolioCard>  
-}
+
+
+/*
+
+* Portfolio pieces added to the arsenal
+todo  title:    Profile Creator for Your Team
+//    github:   https://github.com/lonHeligas/profile-creator-for-your-team
+//    Website:  https://lonheligas.github.io/profile-creator-for-your-team/
+//    tec used: HTML / JS / OOP
+
+todo  Weather U Need to Know
+todo  Day Calendar Appointment Tracker
+todo  Quiz for Geeks and Other People
+todo  Random Password Generator
+todo  Tech of Blogs
+
+*/
+
 
 export default PortfolioPage;
---
+++ src/portfolio-data/ProfileCreatorAppData.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const ProfileCreatorApp = [
+  {title:  "Profile Creator for Your Team"},
+  {gitUrl: "git URL"},
+  {webUrl: "web blurb"},
+  {text: "text blurb"},
+  {img: "image link"}
+]
+
+
+export default ProfileCreatorApp
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+todo  title:    Profile Creator for Your Team
+//    github:   https://github.com/lonHeligas/profile-creator-for-your-team
+//    Website:  https://lonheligas.github.io/profile-creator-for-your-team/
+//    tec used: HTML / JS / OOP
+*/
