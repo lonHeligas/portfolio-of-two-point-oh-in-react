21e9925d2ce62a50d52b8292233d6934f6ad1e73
Lon Heligas <lon.heligas@gmail.com>
2023-03-05 19:21:59 -0600

    adding forms and email verification

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import { useState } from "react"
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import validator from "validator";
+
+
+
+
 
 const ContactPage = (props) => {
+  const [ email, setEmail ] = useState('');
+  const handleUpdateEmail = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  }
+
+
+
+
   return (
-    <Form>
+    <Form style={{ width: '300px '}}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={handleUpdateEmail}/>
         <Form.Text className="text-muted">
           I'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasiAddress">
+      <Form.Group className="mb-3" controlId="formBasicAddress">
         <Form.Label>Address</Form.Label>
         <Form.Control type="address" placeholder="Enter address" />
         <Form.Text className="text-muted"></Form.Text>
