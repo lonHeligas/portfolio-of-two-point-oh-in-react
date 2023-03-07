addce4a5d8f11bb666586b3eeb7739b9ba60f27f
Lon Heligas <lon.heligas@gmail.com>
2023-03-06 21:17:50 -0600

    added portrait image

---
+++ src/components/BioImage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import portrait from "../images/portfolio_portrait.jpg"
+
+
+const BioImage = () => {
+  return(
+    <img style={{ width: 200 }} src={portrait} alt="Portrait of the portfolio author."
+     />
+  )
+}
+
+export default BioImage
+
--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Bio from '../components/Bio'
+import BioImage from '../components/BioImage'
 
 const IntroPage = (props) => {
 
 
   return (
     <>
+      <BioImage />
       <Bio />
     </>
   )
