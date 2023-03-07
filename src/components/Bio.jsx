2194a85928cef60784314996526c253c8a899c7d
Lon Heligas <lon.heligas@gmail.com>
2023-03-06 21:18:58 -0600

    edited text

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -7,10 +7,10 @@ const Bio = () => {
 
   return (
     <>
-      
+
       <h2>Hello there. </h2>
       <h5>About me:</h5>
-      <p>Born in the far-off land of South Dakota, I'd lived there until I was finally aware that yes, I could leave the state. I moved to Minnesota, learning and living in St. Cloud until fate brought me to the Twin Cities.</p>
+      <p>Born in the far-off land of South Dakota, I'd lived there until I was finally aware that yes, they would let me leave the state. I moved to Minnesota, learning and living in St. Cloud until fate brought me to the Twin Cities.</p>
       <p>Going through the past 20 years as a custom eLearning content developer, I'd watched the industry move from Authorware, through Flash, then on to jQuery and finally settle on React.</p>
       <p>(At least for now.)</p>
       <p>My own skills in HTML and CSS were well-devloped in my most recent employment venture, but my JavaScript coding skills were rather lacking. But after a solid thirteen weeks at breakneck speed in the University of Minnesota Coding Boot Camp, I'm ready to dive headlong into JavaScript. Or jQuery. Or even MySQL or Mongo. Throw in some React and I'll be a happy camper.</p>
