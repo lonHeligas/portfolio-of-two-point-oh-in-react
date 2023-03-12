4b77d4bad6bdfc74fd2089d6e18fdd97f32919e4
Lon Heligas <lon.heligas@gmail.com>
2023-03-12 17:13:43 -0500

    footer nearly done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,23 @@
 import * as React from 'react';
 import "../styles/footer.css"
 import Button from 'react-bootstrap/Button';
+import { Github } from 'react-bootstrap-icons';
+import { Mailbox } from 'react-bootstrap-icons';
+import { StackOverflow } from 'react-bootstrap-icons';
+import { Linkedin } from 'react-bootstrap-icons';
+
+
 
 
 const Footer = () => {
 
     return (
-     <div className="footer-bottom" style={{zIndex:1}}>
-      <Button className="footer-button bg-transparent" size="lg">
-          One
-      </Button>      
-      <Button className="footer-button bg-transparent" size="lg">
-          Two
-      </Button>
-        <h3 className="footer-text">Copyright © 2023 Lon Heligas</h3>
-      <Button className="footer-button bg-transparent" size="lg">
-          Three
-      </Button>
-      <Button className="footer-button bg-transparent"size="lg">
-          Four
-      </Button>
-     
+     <div className="footer-bottom" style={{zIndex:2}}>     
+          <Mailbox size="3em" className="footer-icons"/>
+          <Github size="3em" className="footer-icons"/>
+        <h3 className="footer-text">Copyright © 2023 Lon Heligas</h3>     
+          <StackOverflow size="3em" className="footer-icons"/>
+          <Linkedin size="3em" className="footer-icons"/>       
       <div className="footer-buttons">      
       </div>
      </div>
